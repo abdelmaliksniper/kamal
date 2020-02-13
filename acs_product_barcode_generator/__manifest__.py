5ddf62d3f25b2ba85b2aab7466e41134ece8e460
Abdelmalik Yousif <abdelmalik19930@gmail.com>
2020-02-13 11:33:51 +0300

    Add files via upload

---
+++ acs_product_barcode_generator/__manifest__.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+{
+    "name": 'ACS Product Barcode Generator',
+    'summary': 'Hospital Management System',
+    'description': """
+        Hospital Management System
+    """,
+    'version': '1.0.1',
+    'category': 'Medical',
+    'author': 'Abdelmalik Yousif',
+    'support': 'abdelmalik@arsof.com',
+    'website': 'http://www.arsof.co',
+    "depends": ['product'],
+    "data": [
+       "data/data.xml",
+       "views/product_view.xml",
+    ],
+    'images': [
+    ],
+    'installable': True,
+    'sequence': 2,
+}
+# vim:expandtab:smartindent:tabstop=4:softtabstop=4:shiftwidth=4: 
